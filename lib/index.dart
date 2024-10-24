379ac292c018e2297aaf45a88bb5723273cf6ff5
LuEx10 <lucex109@gmail.com>
2024-10-24 13:43:16 +0000

    teste dockerfile

--- lib/index.dart
+++ lib/index.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-String ip_base = "127.0.0.1";
+String ip_base = "192.168.138.138";
 
 validaNull(value) {
   if (value == null || value.isEmpty) {
